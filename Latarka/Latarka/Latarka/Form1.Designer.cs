5da5a3e100882b57a4d9c6d825e39f7d5bea54d6
peter918 <32490004+peter918@users.noreply.github.com>
2018-04-25 03:22:15 +0200

    Add files via upload

---
+++ Latarka/Latarka/Latarka/Form1.Designer.cs
@@ -0,0 +1,210 @@
+﻿namespace Latarka
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button7 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(36, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(292, 277);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(36, 315);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Włacz";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(253, 315);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Wyłacz";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(385, 171);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(120, 23);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Dodaj baterie";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(385, 12);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(120, 23);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "Dodaj żarówke";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(385, 200);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(120, 23);
+            this.button5.TabIndex = 8;
+            this.button5.Text = "Usuń baterie";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(385, 41);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(120, 23);
+            this.button6.TabIndex = 9;
+            this.button6.Text = "Usuń żarówke";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(490, 76);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 10;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(385, 243);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 95);
+            this.listBox1.TabIndex = 11;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(424, 350);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 12;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(360, 76);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(124, 13);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Poziom jasności żarówki:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(389, 139);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 13);
+            this.label4.TabIndex = 14;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(385, 102);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(120, 23);
+            this.button7.TabIndex = 15;
+            this.button7.Text = "Wymień żarówke";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Info;
+            this.ClientSize = new System.Drawing.Size(733, 394);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Latarka";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button7;
+    }
+}
+
---
+++ Latarka/Latarka/Latarka/Form1.cs
@@ -0,0 +1,327 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Latarka
+{
+
+
+    public partial class Form1 : Form
+    {
+        Latarka latarka = new Latarka();
+        int index;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (latarka.getJest() == true)
+            {
+                latarka.wlacz();
+                if (latarka.getCzySwieci() == true)
+                {
+                    pictureBox1.Image = new Bitmap(@"C:\Users\Hesu\Desktop\Latarka\Latarka\Latarka\Latarka_Olight_S30_Baton_XML2_[82916]_1200.jpg");
+                    pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+
+                }
+                else
+                {
+                    pictureBox1.Image = new Bitmap(@"C:\Users\Hesu\Desktop\Latarka\Latarka\Latarka\latarka-taktyczna-fenix_146.jpg");
+                    pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+                }
+            }
+            else
+            {
+                pictureBox1.Image = new Bitmap(@"C:\Users\Hesu\Desktop\Latarka\Latarka\Latarka\latarka-taktyczna-fenix_146.jpg");
+                pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            latarka.setCzySwieci();
+            pictureBox1.Image = new Bitmap(@"C:\Users\Hesu\Desktop\Latarka\Latarka\Latarka\latarka-taktyczna-fenix_146.jpg");
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            latarka.dodajBaterie(new Bateria(rnd.Next(0, 101)));
+            if (latarka.getRozmiar() <= 4)
+            {
+                listBox1.Items.Add(latarka.getBateriaLatarka().ElementAt(latarka.getRozmiar() - 1));
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            index = 0;
+            Random rnd = new Random();
+            latarka.dodajZarowke(new Zarowka(rnd.Next(1, 11)));
+            label1.Text = latarka.getZarowkaLatarka().ElementAt(index).getJasnosc().ToString();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            /*index = listBox1.SelectedIndex;
+            latarka.usunBaterie(index);
+            listBox1.Items.RemoveAt(index);
+            index--;*/
+            if (latarka.getCzySwieci() == false)
+            {
+                listBox1.Items.Clear();
+                index = 0;
+                for (int i = 0; i < latarka.getRozmiar(); i++)
+                {
+                    latarka.usunBaterie(index);
+                }
+                latarka.setRozmiar();
+                label2.Text = null;
+                label4.Text = null;
+            }
+            else
+            {
+                label4.Text = "Prosze najpierw \nwyłaczyc latarke";
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            index = listBox1.SelectedIndex;
+            label2.Text = latarka.getBateriaLatarka().ElementAt(index).getMoc()+" V".ToString();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            latarka.usunZarowke();
+            label1.Text = null;
+
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            if((latarka.getJest() == true) && latarka.getCzySwieci() == false)
+            {
+                index = 0;
+                latarka.usunZarowke();
+                label1.Text = null;
+                Random rnd = new Random();
+                latarka.dodajZarowke(new Zarowka(rnd.Next(1, 11)));
+                label1.Text = latarka.getZarowkaLatarka().ElementAt(index).getJasnosc().ToString();
+                label4.Text = null;
+            }
+            else
+            {
+                label4.Text = "Można wymeinić tylko gdy latarka ma \nwsadzona żarówke i nie jest właczona";
+            }
+        }
+    }
+
+    class Latarka
+    {
+        private List<Bateria> bateriaLatarka = new List<Bateria>();
+        private List<Zarowka> zarowkaLatarka = new List<Zarowka>();
+
+        public List<Bateria> getBateriaLatarka()
+        {
+            return bateriaLatarka;
+        }
+
+        public List<Zarowka> getZarowkaLatarka()
+        {
+            return zarowkaLatarka;
+        }
+
+
+        private int rozmiar=0;
+
+        public void setRozmiar()
+        {
+            this.rozmiar = 0;
+        }
+
+        public int getRozmiar()
+        {
+            return rozmiar;
+        }
+
+        private Boolean jest = false;
+
+        public Boolean getJest()
+        {
+            return jest;
+        }
+
+        //private Boolean czyWlaczona = false; //czy przycisk
+
+        private Boolean czySwieci = false; //czy swieci
+
+        public Boolean getCzySwieci()
+        {
+            return czySwieci;
+        }
+
+        public Boolean setCzySwieci()
+        {
+            return czySwieci = false;
+        }
+
+        /*public void wlacz()
+        {
+            if (czyWlaczona == false)
+            {
+                int zmienna=0;
+                czyWlaczona = true;
+
+                for (int i = 0; i < rozmiar; i++)
+                {
+                    if (bateriaLatarka.ElementAt(i).getMoc() > 15)
+                    {
+                        zmienna++;
+                    }
+                }
+                if (zmienna >= 2)
+                {
+                    czySwieci = true;
+                }
+            }
+        }*/
+
+        public void wlacz()
+        {
+                if ((rozmiar == 2) && (bateriaLatarka.ElementAt(0).getMoc() > 15 && bateriaLatarka.ElementAt(1).getMoc() > 15))
+                {
+                    czySwieci = true;
+                for (int i = 0; i < 2; i++)
+                    bateriaLatarka.ElementAt(i).setMoc(bateriaLatarka.ElementAt(i).getMoc() - zarowkaLatarka.ElementAt(0).getJasnosc());
+                /*bateriaLatarka.ElementAt(0).setMoc(bateriaLatarka.ElementAt(0).getMoc() - zarowkaLatarka.ElementAt(0).getJasnosc());
+                bateriaLatarka.ElementAt(1).setMoc(bateriaLatarka.ElementAt(1).getMoc() - zarowkaLatarka.ElementAt(0).getJasnosc());*/
+            }
+                else if (rozmiar == 3 && (bateriaLatarka.ElementAt(0).getMoc() > 15 && (bateriaLatarka.ElementAt(1).getMoc() > 15 && bateriaLatarka.ElementAt(2).getMoc() > 15)))
+                {
+                    czySwieci = true;
+                for (int i = 0; i < 3; i++)
+                    bateriaLatarka.ElementAt(i).setMoc(bateriaLatarka.ElementAt(i).getMoc() - zarowkaLatarka.ElementAt(0).getJasnosc());
+                /*bateriaLatarka.ElementAt(0).setMoc(bateriaLatarka.ElementAt(0).getMoc() - zarowkaLatarka.ElementAt(0).getJasnosc());
+                bateriaLatarka.ElementAt(1).setMoc(bateriaLatarka.ElementAt(1).getMoc() - zarowkaLatarka.ElementAt(0).getJasnosc());
+                bateriaLatarka.ElementAt(2).setMoc(bateriaLatarka.ElementAt(2).getMoc() - zarowkaLatarka.ElementAt(0).getJasnosc());*/
+            }
+                else if (rozmiar == 4 && (bateriaLatarka.ElementAt(0).getMoc() > 15 && ((bateriaLatarka.ElementAt(1).getMoc() > 15) && (bateriaLatarka.ElementAt(2).getMoc() > 15 && bateriaLatarka.ElementAt(3).getMoc() > 15))))
+                {
+                    czySwieci = true;
+                for(int i=0; i < 4; i++)
+                    bateriaLatarka.ElementAt(i).setMoc(bateriaLatarka.ElementAt(i).getMoc() - zarowkaLatarka.ElementAt(0).getJasnosc());
+                /*bateriaLatarka.ElementAt(0).setMoc(bateriaLatarka.ElementAt(0).getMoc() - zarowkaLatarka.ElementAt(0).getJasnosc());
+                bateriaLatarka.ElementAt(1).setMoc(bateriaLatarka.ElementAt(1).getMoc() - zarowkaLatarka.ElementAt(0).getJasnosc());
+                bateriaLatarka.ElementAt(2).setMoc(bateriaLatarka.ElementAt(2).getMoc() - zarowkaLatarka.ElementAt(0).getJasnosc());
+                bateriaLatarka.ElementAt(3).setMoc(bateriaLatarka.ElementAt(3).getMoc() - zarowkaLatarka.ElementAt(0).getJasnosc());*/
+            }
+                else czySwieci = false;
+        }
+
+        /*public void wlaczLatarke()
+        {
+            this.czyWlaczona = true;
+        }*/
+
+        /*public void wylaczLatarke()
+        {
+            this.czyWlaczona = false;
+        }*/
+
+        public void dodajBaterie(Bateria bateria)
+        {
+            if(rozmiar < 5)
+            {
+                bateriaLatarka.Add(bateria);
+                rozmiar++;
+                //Console.WriteLine("Dodano "+rozmiar+" baterie");
+            }
+        }
+
+        public void usunBaterie(int index)
+        {
+            bateriaLatarka.RemoveAt(index);
+            rozmiar--;
+            /*for (int i = 0; i < rozmiar; i++)
+            {
+                if (bateriaLatarka.ElementAt(i).getMoc() < 15)
+                {
+                    bateriaLatarka.RemoveAt(i);
+                    rozmiar--;
+                }
+            }*/
+        }
+
+        public void dodajZarowke(Zarowka zarowka)
+        {
+            if ( jest == false)
+            {
+                zarowkaLatarka.Add(zarowka);
+                Console.WriteLine("Dodano żarówke");
+                jest = true;
+            }
+            else
+            {
+                Console.WriteLine("Latarka może mieć tylko 1 żarówke");
+            }
+        }
+
+        public void usunZarowke()
+        {
+            zarowkaLatarka.RemoveAt(0);
+            jest = false;
+        }
+    }
+
+    class Bateria
+    {
+        private int moc;
+
+        public Bateria(int moc)
+        {
+            this.moc = moc;
+        }
+
+        public void setMoc(int moc)
+        {
+            this.moc = moc;
+        }
+
+        public int getMoc()
+        {
+            return moc;
+        }
+    }
+
+    class Zarowka
+    {
+        private int jasnosc;
+
+        public Zarowka(int jasnosc)
+        {
+            this.jasnosc = jasnosc;
+        }
+
+        public void setJasnosc(int jasnosc)
+        {
+            this.jasnosc = jasnosc;
+        }
+
+        public int getJasnosc()
+        {
+            return jasnosc;
+        }
+    }
+}
